f783c5c376b0908449edbf0af7585a709ce19e1e
Jerome Russ <jerome.d.russ+c9@gmail.com>
2015-10-23 10:47:13 +0000

    Add POST to Yammer Group

--- routes.js
+++ routes.js
@@ -2,9 +2,10 @@ function setupRouting(){
     yam.getLoginStatus(
       function(response) {
         if (response.authResponse) {
-        var b = document.getElementById("b");
-        b.style.display = 'block';
+          var b = document.getElementById("b");
+          b.style.display = 'block';
 	        var pageApp = document.querySelector("#app");
+	        var drawer = document.querySelector('paper-drawer-panel');
 
           //configure the routes
           page('/', home);
@@ -18,26 +19,32 @@ function setupRouting(){
 
         	function home(){
         		pageApp.route = 'home';
+        		drawer.closeDrawer();
         	}
         
         	function following(){
         		pageApp.route = 'following';
+        		drawer.closeDrawer();
         	}
         
         	function trending(){
         		pageApp.route = 'trending';
+        		drawer.closeDrawer();
         	}
         
         	function tbt(){
         		pageApp.route = 'tbt';
+        		drawer.closeDrawer();
         	}
         
         	function login(){
         		pageApp.route = 'login';
+        		drawer.closeDrawer();
         	}
         
         	function addIdea(){
         		pageApp.route = 'addIdea';
+        		drawer.closeDrawer();
         	}
         } else {
             window.location = '/login.html';
